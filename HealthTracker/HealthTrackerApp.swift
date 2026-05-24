f8f99d31d772df0810c893fe149cee92d4d238e7
erickahr86 <erickahr86@gmail.com>
2026-05-23 23:17:54 -0500

    Initial Commit

---
+++ HealthTracker/HealthTrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker
+//
+//  Created by Erick Herrera on 5/23/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HealthTrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
